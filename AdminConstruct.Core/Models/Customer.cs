06198581360241116d9654ba6570db10d3cc3139
jorge henriquez <jorgeahenriqueznovoa@gmail.com>
2025-11-18 19:46:55 -0500

    feat: proyect Core

---
+++ AdminConstruct.Core/Models/Customer.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminConstruct.Web.Models;
+
+public class Customer
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Document { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+}
---
+++ AdminConstruct.Core/Models/SaleDetail.cs
@@ -0,0 +1,19 @@
+namespace AdminConstruct.Web.Models;
+
+public class SaleDetail
+{
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public Guid SaleId { get; set; }
+    
+    public Sale Sale { get; set; } = null!;
+    
+    public Guid ProductId { get; set; }
+    
+    public Product Product { get; set; } = null!;
+    
+    public int Quantity { get; set; }
+    
+    public decimal UnitPrice { get; set; }
+}
